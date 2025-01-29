52f90aa18c676f618a5bf453a264e0d5e2dd2922
Muhammad Faiz Shahid <mfaizshahid.me@gmail.com>
2025-01-30 01:14:47 +0500

    chore: register page api

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,27 +1,88 @@
-import Form from "next/form";
-import { Description, Field, Input, Label } from "@headlessui/react";
+"use client";
+import { Field, Input, Label } from "@headlessui/react";
 import clsx from "clsx";
+import { useFormik } from "formik";
+import { IAuth } from "@/interfaces";
+import * as Yup from "yup";
 
-export default function Home() {
+const SignupSchema = Yup.object().shape({
+  username: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  email: Yup.string().email("Invalid email").required("Required"),
+  password: Yup.string().min(8, "Too Short!").required("Required").max(16),
+});
+export default function Register() {
+  const registerForm = useFormik<IAuth.RegisterApiPayload>({
+    initialValues: {
+      username: "",
+      email: "",
+      password: "",
+    },
+    validationSchema: SignupSchema,
+    validateOnChange: true,
+    validateOnBlur: true,
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Form action="" className="flex flex-col gap-4">
-          <Field>
-            <Label className="text-sm/6 font-medium text-white">Name</Label>
-            <Description className="text-sm/6 text-white/50">
-              Use your real name so people will recognize you.
-            </Description>
+        <form
+          onSubmit={registerForm.handleSubmit}
+          className="flex flex-col gap-4"
+        >
+          <Field className="mb-5">
+            <Label className="text-sm/6 font-medium">Username</Label>
             <Input
-              className={clsx(
-                "mt-3 block w-full rounded-lg border-none bg-white/5 py-1.5 px-3 text-sm/6 text-white",
-                "focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25"
-              )}
+              name="username"
+              type="text"
+              placeholder="Username"
+              className="mt-1 block w-full rounded-lg border  py-1.5 px-3"
+              onChange={registerForm.handleChange}
+              value={registerForm.values.username}
             />
           </Field>
-          <Input name="email" type="email" />
-          <Input name="password" type="password" />
-        </Form>
+          <Field className="mb-5">
+            <Label className="text-sm/6 font-medium">Email</Label>
+            <Input
+              name="email"
+              type="email"
+              placeholder="Email"
+              className="mt-1 block w-full rounded-lg border  py-1.5 px-3"
+              onChange={registerForm.handleChange}
+              value={registerForm.values.email}
+            />
+          </Field>
+          <Field className="mb-5">
+            <Label className="text-sm/6 font-medium">
+              {registerForm.errors.password}
+            </Label>
+            <Input
+              placeholder="Password"
+              name="password"
+              type="password"
+              className="mt-1 block w-full rounded-lg border  py-1.5 px-3"
+              onChange={registerForm.handleChange}
+              value={registerForm.values.password}
+            />
+          </Field>
+          <button
+            type="submit"
+            className={clsx(
+              "bg-black text-white py-2.5 px-5 rounded-lg font-medium",
+              {
+                "cursor-not-allowed bg-gray-300 text-black":
+                  !registerForm.isValid,
+              }
+            )}
+            disabled={!registerForm.isValid}
+          >
+            Register
+          </button>
+        </form>
       </main>
     </div>
   );
